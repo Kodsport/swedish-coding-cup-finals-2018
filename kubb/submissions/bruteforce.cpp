b51295992b90814d8467cd6ac90e74b27a86455e
Simon Lindholm <simon.lindholm10@gmail.com>
2018-06-02 11:34:51 +0200

    kubb: statement, sample

---
+++ kubb/submissions/bruteforce.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+const double inf = 1.0 / 0.0;
+
+double A, B, C;
+
+double bruteforce(int N, bool ignMiddle = false) {
+	double d = 1 - A - B - C;
+	assert(0 <= A);
+	assert(0 <= B);
+	assert(0 <= C);
+	assert(-1e-9 <= d);
+	assert(d < 1);
+	assert(N < 30);
+	int canhit = (A > 0) | (B > 0) << 1 | (C > 0) << 2;
+
+	vector<double> dp(1 << N);
+	rep(mask, 1, (1 << N)) {
+		int mask2 = mask << 2;
+		double res = inf;
+		int first = -1, last = - 1;
+		if (ignMiddle) rep(i,0,N) if (mask & (1 << i)) {
+			if (first == -1) first = i;
+			last = i;
+		}
+		rep(i,-1,N+1) if (mask2 & (canhit << (i+1))) {
+			if (ignMiddle && first + 3 < i && i < last - 3) continue;
+			double miss = d;
+			double e2 = 0;
+			if (mask2 & (1 << (i+1))) e2 += A * dp[mask &~ (1 << (i - 1))];
+			else miss += A;
+			if (mask2 & (1 << (i+2))) e2 += B * dp[mask &~ (1 << (i + 0))];
+			else miss += B;
+			if (mask2 & (1 << (i+3))) e2 += C * dp[mask &~ (1 << (i + 1))];
+			else miss += C;
+			assert(miss < 1);
+			// e = 1 + e2 + miss * e
+			double e = (1 + e2) / (1 - miss);
+			res = min(res, e);
+		}
+		dp[mask] = res;
+	}
+	return dp.back();
+}
+
+int main() {
+	int N;
+	cin >> N;
+	double a, b, c;
+	cin >> a >> b >> c;
+	::A = a;
+	::B = b;
+	::C = c;
+	cout << setprecision(10) << fixed << bruteforce(N) << endl;
+}

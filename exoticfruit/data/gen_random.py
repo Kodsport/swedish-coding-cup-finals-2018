b8fe688054691f287ffd1ff9db77bd030ab5d164
Nicole Hedblom <nhedblom@kth.se>
2018-06-05 23:42:41 +0200

    Some randint and some randrange

--- exoticfruit/data/gen_random.py
+++ exoticfruit/data/gen_random.py
@@ -3,12 +3,13 @@
 import sys
 import random
 
+
 def cmdlinearg(name, default=None):
     for arg in sys.argv:
         if arg.startswith(name + "="):
             return arg.split("=")[1]
     assert default is not None, name
-return default
+	return default
 
 random.seed(int(cmdlinearg('seed', sys.argv[-1])))
 n = int(cmdlinearg('n'))
@@ -20,9 +21,9 @@ dlim = int(cmdlinearg('d'))
 fruits = []
 
 for _ in range(k-s):
-	b = random.randrange(0, blim)
-	c = random.randrange(0, 1e9+1)
-	d = random.randrange(0, dlim)
+	b = random.randint(0, blim)
+	c = random.randint(0, 1e9)
+	d = random.randint(0, dlim)
 	fruits.append[(b,c,d)]
 
 for _ in range(k-s, k):

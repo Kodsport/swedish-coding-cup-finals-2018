8c7934b56127402d180886a173d7b456eba109d5
Simon Lindholm <simon.lindholm10@gmail.com>
2018-06-06 10:38:57 +0200

    moonfrostcanyon: fix generator

--- moonfrostcanyon/data/generator.sh
+++ moonfrostcanyon/data/generator.sh
@@ -15,12 +15,12 @@ limits r=1000 c=1000 mode=general
 #sample 2
 
 group group1 10
-limits r=1000 c=1000 mode=tilled
-tc empty-1 gen_norock r=1000 c=3
-tc empty-2 gen_norock r=3 c=1000
-tc empty-3 gen_norock r=1000 c=6
-tc empty-4 gen_norock r=6 c=1000
-tc empty-5 gen_norock r=1000 c=1000
+limits r=999 c=999 mode=tilled
+tc empty-1 gen_norock r=999 c=3
+tc empty-2 gen_norock r=3 c=996
+tc empty-3 gen_norock r=996 c=6
+tc empty-4 gen_norock r=6 c=999
+tc empty-5 gen_norock r=999 c=999
 
 group group2 10
 limits r=6 c=1000 mode=r6
@@ -42,7 +42,8 @@ tc random-4 gen_random r=1000 c=1000 dens=80
 tc random-5 gen_random r=1000 c=1000 dens=90
 tc random-6 gen_random r=1000 c=1000 dens=90
 tc random-7 gen_random r=1000 c=1000 dens=90
-tc random-8 gen_random r=1000 c=1000 dens=95
-tc random-9 gen_random r=1000 c=1000 dens=95
+tc random-8 gen_random r=1000 c=1000 dens=130
+tc random-9 gen_random r=1000 c=1000 dens=130
 
 # TODO: more connected cases
+cleanup_programs
